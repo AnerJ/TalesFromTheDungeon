5fb23f6a132b074932446a67d99dab26183567d4
ruBBBBBBa <ruben.garcia.saiz@opendeusto.es>
2025-03-18 17:20:29 +0100

    implementacion carpetas sqlite

--- personaje.h
+++ personaje.h
@@ -3,6 +3,20 @@
 
 void crearPersonaje();
 
+typedef struct Personaje
+{
+    int ataque = 0;
+    int armadura = 0;
+    int vida = 0;
+    int velocidad = 0;
+    int precision = 0;
+    int clase = 0;
+    int nivel = 0;
+    
+};
+
+
+
 
 
 #endif
